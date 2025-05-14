697ec1c0063316c9053d26a7ef2e5364f06cb3ed
Chase-droid <chasecui.96@gmail.com>
2025-05-14 18:22:13 +1200

    Delete backend directory

--- backend/src/main/java/com/talentconnect/backend/model/ApplicationStatus.java
+++
@@ -1,9 +0,0 @@
-package com.talentconnect.backend.model;
-
-public enum ApplicationStatus {
-    SUBMITTED,
-    REVIEWING,
-    ACCEPTED,
-    REJECTED,
-    WITHDRAWN
-}
--- backend/src/main/java/com/talentconnect/backend/model/RoleType.java
+++
@@ -1,6 +0,0 @@
-package com.talentconnect.backend.model;
-
-public enum RoleType {
-    JOBSEEKER,
-    EMPLOYER
-}
